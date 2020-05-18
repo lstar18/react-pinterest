76d4bfc2996c08c1d2c039fa8901046aea0d4c7f
Liza Star <lizadstar18@gmail.com>
2020-05-18 18:32:50 -0500

    added inital components

---
+++ src/components/BoardContainer/BoardContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './BoardContainer.scss';
+
+class BoardContainer extends React.Component {
+  render() {
+    return (
+      <div className="BoardContainer">
+        <h1> Board Container </h1>
+      </div>
+    );
+  }
+}
+
+export default BoardContainer;
