f9a76b7a20f540cfd8db98487c80500475fd07df
Liza Star <lizadstar18@gmail.com>
2020-05-18 19:11:33 -0500

    login works

--- src/App/App.js
+++ src/App/App.js
@@ -1,17 +1,48 @@
 import React from 'react';
-import './App.scss';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 import Auth from '../components/Auth/Auth';
 import BoardContainer from '../components/BoardContainer/BoardContainer';
 import MyNavBar from '../components/MyNavBar/MyNavBar';
+import fbConnection from '../helpers/data/connection';
+import './App.scss';
 
+fbConnection();
 class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
   render() {
+    const { authed } = this.state;
+    const loadComponent = () => {
+      let componentToLoad = '';
+      if (authed) {
+        componentToLoad = <BoardContainer />;
+      } else {
+        componentToLoad = <Auth />;
+      }
+      return componentToLoad;
+    };
     return (
       <div className="App">
-        <h2>INSIDE APP COMPONENT</h2>
+        <h2>React Pinterest </h2>
         <MyNavBar />
-        <Auth />
-        <BoardContainer />
+        {loadComponent()}
       </div>
     );
   }
