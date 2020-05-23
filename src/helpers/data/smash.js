94335bca5de8eddacfcf19b06ba92a2c94fb0edb
Liza Star <lizadstar18@gmail.com>
2020-05-23 09:48:57 -0500

    completely removed board

---
+++ src/helpers/data/smash.js
@@ -0,0 +1,16 @@
+import boardsData from './boardsData';
+import pinsData from './pinsData';
+
+const completelyRemoveBoard = (boardId) => new Promise((resolve, reject) => {
+  boardsData.deleteBoard(boardId)
+    .then(() => {
+      pinsData.getPinsbyBoardId(boardId)
+        .then((pins) => {
+          pins.forEach((pin) => pinsData.deletePin(pin.id));
+          resolve();
+        });
+    })
+    .catch((err) => reject(err));
+});
+
+export default { completelyRemoveBoard };
